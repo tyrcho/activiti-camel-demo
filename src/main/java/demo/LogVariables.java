987210979b91dc2c586cad707723e312d5a107e3
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-04-16 14:41:11 +0200

    sample call from activiti to camel
    with request and response as properties
    copied to/from activiti variables

---
+++ src/main/java/demo/LogVariables.java
@@ -0,0 +1,18 @@
+package demo;
+
+import java.util.Map.Entry;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class LogVariables implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		System.out.println("in activiti");
+		for (Entry<String, Object> entry : execution.getVariables().entrySet()) {
+			System.out.println(entry.getKey() + " => " + entry.getValue());
+		}
+	}
+
+}
